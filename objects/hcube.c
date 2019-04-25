4bfbe2a6ec16aea9557cf1a0b49a86b4e8d2a98c
Bryan Franklin <44486392+fra99le@users.noreply.github.com>
2019-04-25 17:09:20 -0400

    Updated to use orthotopes instead of hplanes.

--- objects/hcube.c
+++ objects/hcube.c
@@ -12,36 +12,160 @@
 
 static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
+static int factorial(int n) {
+    int ret = 1;
+    for(int i=1; i<=n; ++i) {
+        ret *= i;
+    }
+    return ret;
+}
+
+static int choose(int n, int m) {
+    return factorial(n) / (factorial(m) * factorial(n-m));
+}
+
+/* see: https://en.wikipedia.org/wiki/Hypercube#Elements */
+static int num_n_faces(int n, int m) {
+    /* for an n-cube, how many m-dimensional components does it contain */
+    return (1<<(n-m)) * choose(n, m);
+}
+
+static int add_faces(object *cube, int m) {
+    int n = cube->dimensions;
+    int num_faces = num_n_faces(n, m);
+    vectNd dir;
+    vectNd_calloc(&dir, n);
+    vectNd pos;
+    vectNd_calloc(&pos, n);
+
+    if( m > 2 )
+        add_faces(cube, m-1);
+
+    printf("Adding %i-dimensional faces.\n", m);
+
+    int *dirs_count, *pos_count;
+    dirs_count = calloc(m, sizeof(int));
+    pos_count = calloc(n-m, sizeof(int));
+    for(int i=0; i<n; ++i)
+        dirs_count[i] = m-i-1;
+    int offset_id = 0;
+    int real_offset_id = 0;
+    for(int f=0; f<num_faces; ++f) {
+        #if 0
+        printf("\ndirs_count: ");
+        for(int i=0; i<m; ++i) {
+            printf(" %i", dirs_count[i]);
+        }
+        printf("\npos_count: ");
+        for(int i=0; i<n-m; ++i) {
+            printf(" %i", pos_count[i]);
+        }
+        printf("\n");
+        printf("real_offset_id = %i\n", real_offset_id);
+        #endif /* 0 */
+
+        vectNd_reset(&pos);
+        vectNd_reset(&dir);
+        offset_id = real_offset_id;
+        for(int i=0; i<n; ++i) {
+            /* determine which dimensions 'face' exists in */
+            int is_dir = 0;
+            for(int j=0; is_dir==0 && j<m; ++j) {
+                if( i == dirs_count[j] )
+                    is_dir = 1;
+            }
+            if( is_dir ) {
+                vectNd_set(&pos, i, cube->pos[0].v[i] - 0.5 * cube->size[i]);
+                continue;
+            }
+
+            int value = offset_id % 2;
+            offset_id >>= 1;
+
+            /* determine location of 'face' */
+            vectNd_set(&pos, i, cube->size[i] * (value-0.5));
+        }
+
+        object *obj = NULL;
+        if( m > 1 ) {
+            /* add a orthotope for face */
+            obj = object_alloc(cube->dimensions, "orthotope", "");
+            object_add_flag(obj, m);
+            if( m == 3 || n == 3 ) {
+                obj->red = 0.0;
+                obj->green = 0.0;
+                obj->blue = 0.8;
+            } else {
+                obj->red = 0.8;
+                obj->green = 0.8;
+                obj->blue = 0.8;
+            }
+
+            for(int i=0; i<m; ++i) {
+                int j = dirs_count[i];
+                vectNd_set(&pos, j, cube->pos[0].v[j] - 0.5 * cube->size[j]);
+                vectNd_reset(&dir);
+                vectNd_set(&dir, j, cube->size[j]);
+                object_add_dir(obj, &dir);
+                #if 0
+                vectNd_print(&dir,"dir");
+                #endif /* 0 */
+            }
+            object_add_pos(obj, &pos);
+            #if 0
+            vectNd_print(&pos,"pos");
+            #endif /* 0 */
+        } else {
+            fprintf(stderr, "%i-dimensional face shouldn't be requested.\n", m);
+            exit(1);
+        }
+        snprintf(obj->name, sizeof(obj->name), "%id face %i of %s", m, f, cube->name);
+        object_add_obj(cube, obj);
+
+        /* update counters */
+        ++real_offset_id;
+        int i=0;
+        while(pos_count[i] == 1) {
+            pos_count[i] = 0;
+            ++i;
+        }
+        if( i < n-m ) {
+            pos_count[i] += 1;
+        } else {
+            /* pos counter reached end, so update dirs_counter */
+            int j=0;
+            while(dirs_count[j] == n-j-1 ) {
+                if( j < m-1 ) {
+                    dirs_count[j] = dirs_count[j+1]+1;
+                } else {
+                    dirs_count[j] = 0;
+                }
+                ++j;
+            }
+            if( j < m ) {
+                dirs_count[j] += 1;
+                --j;
+                while( j >= 0 ) {
+                    dirs_count[j] = dirs_count[j+1]+1;
+                    --j;
+                }
+            }
+        }
+    }
+    free(dirs_count); dirs_count=NULL;
+    free(pos_count); pos_count=NULL;
+
+    return 0;
+}
+
+
 static int prepare(object *hcube)
 {
     pthread_mutex_lock(&lock);
 
     /* fill in any ray invarient parameters */
     if( !hcube->prepared ) {
-
-        object *face;
-        vectNd zero;
-        vectNd_calloc(&zero,hcube->dimensions);
-        for(int i=0; i<hcube->n_size; i++) {
-            /* positive face */
-            face = object_alloc(hcube->dimensions, "hplane", "face a");
-            object_add_pos(face, &zero);
-            object_add_dir(face, &zero);
-            vectNd_set(&face->pos[0],i,hcube->size[i]/2);
-            vectNd_set(&face->dir[0],i,1);
-            object_move(face, &hcube->pos[0]);
-            object_add_obj(hcube, face);
-
-            /* negative face */
-            face = object_alloc(hcube->dimensions, "hplane", "face b");
-            object_add_pos(face, &zero);
-            object_add_dir(face, &zero);
-            vectNd_set(&face->pos[0],i,-hcube->size[i]/2);
-            vectNd_set(&face->dir[0],i,-1);
-            object_move(face, &hcube->pos[0]);
-            object_add_obj(hcube, face);
-        }
-        vectNd_free(&zero);
+        add_faces(hcube, hcube->dimensions-1);
 
         /* mark object as prepared */
         hcube->prepared = 1;
@@ -74,22 +198,13 @@ int get_bounds(object *obj) {
         prepare(obj);
     }
 
-    /* get centroid of all plane defining points */
-    for(int i=0; i<obj->n_obj; i++) {
-        vectNd_add(&obj->bounds.center,&obj->obj[i]->pos[0],
-                    &obj->bounds.center);
-    }
-    vectNd_scale(&obj->bounds.center,1.0/obj->n_obj,&obj->bounds.center);
+    /* center of hcube is center of bounding sphere */
+    vectNd_copy(&obj->bounds.center, &obj->pos[0]);
 
-    /* get radius */
-    double sum=0;
-    for(int i=0; i<obj->n_obj; i++) {
-        double dist;
-        vectNd_dist(&obj->bounds.center, &obj->obj[i]->pos[0], &dist);
-        sum += dist*dist;
+    double sum = 0;
+    for(int i=0; i<obj->n_size; ++i) {
+        sum += pow(obj->size[i]/2.0, 2.0);
     }
-    /* each dimension will be added twice */
-    sum /= 2;
     obj->bounds.radius = sqrt(sum) + EPSILON;
 
     return 1;
@@ -97,68 +212,14 @@ int get_bounds(object *obj) {
 
 int intersect(object *hcube, vectNd *o, vectNd *v, vectNd *res, vectNd *normal, object **ptr)
 {
-    double best_dist = -1;
-    vectNd lres;
-    vectNd lnorm;
-    int dim = -1;
-    vectNd toInt;
-    vectNd toPoint;;
-    double dProd1 = -1;
-    double dProd2 = -1;
-    int ret = 0;
-
     if( !hcube->prepared ) {
         prepare(hcube);
     }
 
-    dim = o->n;
-    vectNd_alloc(&lres,dim);
-    vectNd_alloc(&lnorm,dim);
-    vectNd_alloc(&toInt,dim);
-    vectNd_alloc(&toPoint,dim);
-
-    /* for each face */
-    for(int i=0; i<hcube->n_obj; ++i) {
-        int lret = 0;
-        /* check for intersection with each face */
-        if( (lret = hcube->obj[i]->intersect(hcube->obj[i],o,v,&lres,&lnorm,ptr))==0 )
-            continue;
-
-        double ldist = -1;
-        vectNd_dist(&lres,o,&ldist);
-        if( ldist < best_dist || best_dist < 0 ) {
-            /* check to see if intersection is on correct side of all other
-             * faces */
-            int valid = 1;
-            for(int j=0; j<hcube->n_obj && valid!=0; ++j) {
-                if( i==j )
-                    continue;
-
-                /* verify that lres is on correct side of face */   
-                vectNd_sub(&lres,
-                    &hcube->obj[j]->pos[0],&toInt);
-                vectNd_sub(&hcube->obj[i]->pos[0],
-                    &hcube->obj[j]->pos[0],&toPoint);
-                vectNd_dot(&hcube->obj[j]->dir[0],&toInt,&dProd1);
-                vectNd_dot(&hcube->obj[j]->dir[0],&toPoint,&dProd2);
-                if( (dProd1*dProd2) < 0 )
-                    valid = 0;
-            }
-
-            if( valid!=0 ) {
-                vectNd_copy(res,&lres);
-                vectNd_copy(normal,&lnorm);
-                best_dist = ldist;
-                ret = 1;
-            }
-        }
-    }
-    vectNd_free(&toInt);
-    vectNd_free(&toPoint);
-    vectNd_free(&lres);
-    vectNd_free(&lnorm);
+    int ret = trace(o, v, hcube->obj, hcube->n_obj, res, normal, ptr);
 
     if( ret && ptr != NULL ) {
+        /* set object to hcube itself for material looks */
         *ptr = hcube;
     }
 
