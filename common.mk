d1380bc618a9c55b669377c8915bbb2538659a2e
Bryan Franklin <44486392+fra99le@users.noreply.github.com>
2019-04-28 14:21:57 -0400

    Added more linking flags to placate travis.

--- common.mk
+++ common.mk
@@ -32,7 +32,7 @@ LDFLAGS+=-g
 UNAME_S := $(shell uname -s)
 ifeq ($(UNAME_S),Linux)
     CFLAGS+=-D_GNU_SOURCE -std=c99
-    LDFLAGS+=-lpthread -ldl -export-dynamic
+    LDFLAGS+=-lm -lyaml -lpthread -ldl -export-dynamic
     DEP_CMD=sudo apt-get install
     DEPS=libjpeg-dev libpng-dev libyaml-dev
 endif
