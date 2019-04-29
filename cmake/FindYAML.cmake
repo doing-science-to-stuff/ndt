84f669f59a5f967fd2606cf262af7938d2542e6f
Bryan Franklin <44486392+fra99le@users.noreply.github.com>
2019-04-29 16:33:06 -0400

    Added find_library calls for extra robustness.

--- cmake/FindYAML.cmake
+++ cmake/FindYAML.cmake
@@ -1,5 +1,7 @@
 find_path(YAML_INCLUDE_DIR yaml.h)
 find_library(YAML_LIBRARY libyaml)
+find_library(YAML_LIBRARY libyaml.so)
+find_library(YAML_LIBRARY libyaml.dylib)
 if( NOT ${YAML_LIBRARY} MATCHES "-NOTFOUND" )
     set(YAML_FOUND TRUE)
 endif()
