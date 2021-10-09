90b77bf4b9d319c585273bea4f22d541edbbc182
Bryan Franklin <44486392+fra99le@users.noreply.github.com>
2021-10-09 17:11:17 -0400

    Added skeleton of kd-tree functions with trivial functions filled in.

---
+++ kd-tree.c
@@ -0,0 +1,161 @@
+#include "kd-tree.h"
+
+/* aabb */
+
+int aabb_init(aabb_t *bb, int dimensions) {
+    vectNd_calloc(bb->lower, dimensions);
+    vectNd_calloc(bb->upper, dimensions);
+}
+
+int aabb_free(aabb_t *bb) {
+    vectNd_free(bb->lower);
+    vectNd_free(bb->lower);
+}
+
+int aabb_add(aabb_t *dst, aabb_t *src) {
+    int dimensions = dst->lower.n;
+    for(int i=0; i<dimensions; ++i) {
+        double vl_d, vu_d, vl_s, vu_s;
+        vectNd_get(&dst->lower, i, &vl_d);
+        vectNd_get(&dst->upper, i, &vu_d);
+        vectNd_get(&src->lower, i, &vl_s);
+        vectNd_get(&src->upper, i, &vu_s);
+
+        if( vl_s < vl_d ) {
+            vl_d = vl_s;
+            vectNd_set(&dst->lower, i, &vl_d);
+        }
+        if( vu_s > vu_d ) {
+            vu_d = vu_s;
+            vectNd_set(&dst->upper, i, &vu_d);
+        }
+    }
+}
+
+/* kd_item */
+
+int kd_item_init(kd_item_t *item, int dimensions) {
+    memset(item, '\0', sizeof(kd_item_t));
+    aabb_init(&item->bb, dimensions);
+    return 1;
+}
+
+kd_item_free(kd_item_t *item) {
+    aabb_free(&item->bb);
+    memset(item, '\0', sizeof(kd_item_t));
+    return 1;
+}
+
+int kd_item_copy(kd_item_t *dst, kd_item_t *src) {
+    vectNd_copy(&dst->bb.lower, src->bb.lower);
+    vectNd_copy(&dst->bb.upper, src->bb.upper);
+    dst->ptr = src->ptr;
+    return 1;
+}
+
+/* kd_node */
+
+int kd_node_init(kd_node_t *node, int dimensions) {
+    memset(node, '\0', sizeof(kd_node_t));
+    node>dim = -1; /* mark as unspecified */
+    node->items = NULL;
+    node->num_items = 0;
+    vectNd_calloc(&node->bb.lower, dimensions);
+    vectNd_calloc(&node->bb.upper, dimensions);
+    return 1;
+}
+
+int kd_node_free(kd_node_t *node) {
+    vectNd_free(node->bb.lower);
+    vectNd_free(node->bb.upper);
+    return 1;
+}
+
+/* kd_tree */
+
+static int kd_tree_free_node(kd_node_t *node) {
+    if( !node )
+        return 0;
+
+    if( node->left ) {
+        kd_tree_free_node(node->left);
+        node->left = NULL;
+    }
+    if( node->right ) {
+        kd_tree_free_node(node->right);
+        node->right = NULL;
+    }
+
+    kd_node_free(node); node=NULL;
+    return 1;
+}
+
+int kd_tree_init(kd_tree_t *tree) {
+    memset(tree, '\0', sizeof(*tree));
+    tree->root = calloc(1, sizeof(kd_node_t));
+    kd_node_init(tree->root);
+    return 1;
+}
+
+int kd_tree_free(kd_tree_t *tree) {
+    kd_tree_free_node(tree->root);
+    tree->root = NULL;
+    return 1;
+}
+
+static int kd_tree_split_node(kd_node_t *node, int dimensions, int levels_remaining, int min_per_node) {
+
+    if( levels_remaining <= 0 || node->num_items < min_per_node )
+        return 1;
+
+    /* pick split point */
+
+    /* make child nodes */
+    node->left = calloc(1, sizeof(kd_node_t));
+    node->right = calloc(1, sizeof(kd_node_t));
+
+    /* set bounding boxes for child nodes */
+
+    /* assign items to child nodes */
+
+    /* recurse */
+    if( node->left->num_items < node->num_items )
+        kd_tree_split_node(node->left, dimensions, levels_remaining-1, min_per_node);
+    if( node->right->num_items < node->num_items )
+        kd_tree_split_node(node->right, dimensions, levels_remaining-1, min_per_node);
+
+    return 0;
+}
+
+int kd_tree_build(kd_tree_t *tree, kd_item_t *items, int n, int dimensions) {
+    /* populate root node with all items */
+    for(int i=0; i<n; ++i) {
+        kd_item_copy(&tree->root->items[i], items[i]);
+        aabb_add(&tree->root->bb, &items[i].bb);
+    }
+
+    /* recursively split root node */
+    return kd_tree_split_node(tree->root, dimensions, -1, -1);
+}
+
+static int kd_node_intersect(kd_node_t *node, vectNd *o, vectNd *v, kd_item_t *items, int *n) {
+    /* check for intersection with bb */
+
+    /* return if not intersected */
+
+    if( node->left==NULL && node->right==NULL ) {
+        /* is a leaf, copy leaf items into items list. */
+    } else {
+        /* otherwise, call for both children */
+        kd_node_intersect(node->left, o, v, items, n);
+        kd_node_intersect(node->right, o, v, items, n);
+    }
+
+    return 0;
+}
+
+int kd_tree_intersect(kd_tree_t *tree, vectNd *o, vectNd *v, kd_item_t *items, int *n) {
+    /* find all leaf nodes that ray o+x*v cross, and return items they contain */
+    return kd_node_intersect(tree->root, o, v, items, n);
+}
+
---
+++ kd-tree.h
@@ -0,0 +1,53 @@
+#ifndef KD_TREE_H
+#define KD_TREE_H
+
+#include "vectNd.h"
+#include "object.h"
+
+/* aabb */
+
+typedef struct aabb {
+    vectNd lower, upper;
+} aabb_t;
+
+int aabb_init(aabb_t *bb, int dimensions);
+int aabb_free(aabb_t *bb);
+int aabb_add(aabb_t *dst, aabb_t *src);
+
+/* kd_item */
+
+typedef struct kd_item {
+    aabb_t bb;
+    void *ptr;
+} kd_item_t;
+
+int kd_item_init(kd_item_t *item, int dimensions);
+int kd_item_free(kd_item_t *item);
+int kd_item_copy(kd_item_t *dst, kd_item_t *src);
+
+/* kd_node */
+
+typedef struct kd_node {
+    aabb_t bb;
+    struct kd_node *left, *right;
+    int dim;
+    double boundary;
+    kd_item_t *items;
+    int num_items;
+} kd_node_t;
+
+int kd_node_init(kd_node_t *node);
+int kd_node_free(kd_node_t *node);
+
+/* kd_tree */
+
+typedef struct kd_tree {
+    kd_node_t *root;
+} kd_tree_t;
+
+int kd_tree_init(kd_tree_t *tree);
+int kd_tree_free(kd_tree_t *tree);
+int kd_tree_build(kd_tree_t *tree, object *objs, int n);
+int kd_tree_intersect(kd_tree_t *tree, vectNd *o, vectNd *v, object *objs, int n, object **objs, int *n);
+
+#endif /* KD_TREE_H */
